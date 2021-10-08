42ec83138352f8338222e84bf2e884ddcd061d39
User <User@DESKTOP-VMHFV0H>
2021-10-08 10:47:38 +0200

    Create Excel Workbook

--- ExcelExport_TQC5OX/ExcelExport_TQC5OX/Form1.cs
+++ ExcelExport_TQC5OX/ExcelExport_TQC5OX/Form1.cs
@@ -16,12 +16,16 @@ namespace ExcelExport_TQC5OX
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
 
         }
 
@@ -29,5 +33,36 @@ namespace ExcelExport_TQC5OX
         {
             lakasok = context.Flats.ToList();
         }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false,Type.Missing,Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        private void CreateTable()
+        {
+
+        }
+
+
     }
 }
