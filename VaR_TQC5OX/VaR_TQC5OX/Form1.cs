887089fea8575183b3da71e9def1be7133526845
User <User@DESKTOP-VMHFV0H>
2021-10-22 10:35:03 +0200

    Add Portfolio

--- VaR_TQC5OX/VaR_TQC5OX/Form1.cs
+++ VaR_TQC5OX/VaR_TQC5OX/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR_TQC5OX.Entities;
 
 namespace VaR_TQC5OX
 {
@@ -14,12 +15,29 @@ namespace VaR_TQC5OX
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolios = new List<PortfolioItem>();
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            //PortfolioItem p = new PortfolioItem();
+            //p.Index = "OTP";
+            //p.Volume = 10;
+            //Portfolios.Add(p); -->
+
+            Portfolios.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolios.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolios.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolios;
         }
     }
 }
