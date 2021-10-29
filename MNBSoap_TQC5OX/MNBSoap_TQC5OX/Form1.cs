663962404b81721f33c609dc6a820e9be94d17ca
User <User@DESKTOP-VMHFV0H>
2021-10-29 10:29:21 +0200

    xml

---
+++ MNBSoap_TQC5OX/MNBSoap_TQC5OX/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MNBSoap_TQC5OX.Entities;
+using MNBSoap_TQC5OX.MNBServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MNBSoap_TQC5OX
+{
+    public partial class Form1 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string xmlstring = Comsume();
+            LoadXml(xmlstring);
+
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild;
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit!=0)
+                {
+                    r.Value = r.Value / unit;
+                    Rates.Add(r);
+                }
+                Rates.Add(r);
+            }
+        }
+
+        private string Comsume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+
+            return result;
+            //File.WriteAllText("export.xml", result);
+        }
+    }
+}
