6f14e0e5be7c1931bf3f94822ccc390f1e14174d
User <User@DESKTOP-VMHFV0H>
2021-10-29 10:39:18 +0200

    Chart Add

--- MNBSoap_TQC5OX/MNBSoap_TQC5OX/Form1.cs
+++ MNBSoap_TQC5OX/MNBSoap_TQC5OX/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MNBSoap_TQC5OX
@@ -26,6 +27,26 @@ namespace MNBSoap_TQC5OX
             LoadXml(xmlstring);
 
             dataGridView1.DataSource = Rates;
+
+            Charting();
+        }
+
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void LoadXml(string input)
@@ -59,8 +80,8 @@ namespace MNBSoap_TQC5OX
             request.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
-
             return result;
+
             //File.WriteAllText("export.xml", result);
         }
     }
