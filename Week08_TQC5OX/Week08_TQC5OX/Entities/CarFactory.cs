818ca4477c22c5e75ab6567719b0d88dcb420d46
User <User@DESKTOP-VMHFV0H>
2021-11-12 12:50:45 +0100

    Add CarFactory class

--- Week08_TQC5OX/Week08_TQC5OX/Entities/CarFactory.cs
+++ Week08_TQC5OX/Week08_TQC5OX/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using Week08_TQC5OX.Abstractions;
 
 namespace Week08_TQC5OX.Entities
 {
-    public class BallFactory : IToyFactory
+   public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
