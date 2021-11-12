2f1661a62a54ce4c5d0776218f4dfd9f9a0122a5
User <User@DESKTOP-VMHFV0H>
2021-11-12 12:42:55 +0100

    Add IToyFactory interface

---
+++ Week08_TQC5OX/Week08_TQC5OX/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week08_TQC5OX.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
