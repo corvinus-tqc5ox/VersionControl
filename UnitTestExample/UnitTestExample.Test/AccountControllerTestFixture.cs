efbd1b3e0026f22796455c4ac06328d18f5d35d9
User <User@DESKTOP-VMHFV0H>
2021-11-26 10:50:26 +0100

    TestValidatePassword

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -29,5 +29,27 @@ namespace UnitTestExample.Test
             Assert.AreEqual(result, expectedResult);
 
         }
+
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcdABCD", false),
+            TestCase("abCD12", false),
+            TestCase("abCD1234", true),
+            TestCase("abCDEF1234", false)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var result = accountController.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+
+        }
     }
 }
