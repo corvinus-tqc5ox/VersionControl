9135c5c83dba9b460dc5aac2e687f49b1d741374
User <User@DESKTOP-VMHFV0H>
2021-11-26 10:33:21 +0100

    .

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,21 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
-        public void TestValidateEmail(string email, bool exptectedResult)
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             //Arrange
             var accountController = new AccountController();
 
-
             //Act
+            var result = accountController.ValidateEmail(email);
 
             //Assert
+            Assert.AreEqual(result, expectedResult);
+
         }
     }
 }
