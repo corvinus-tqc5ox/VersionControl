3b69071d0e5fe59bb0e53cb7aa9b0ebe7d23b261
User <User@DESKTOP-VMHFV0H>
2021-12-03 10:49:12 +0100

    sim

--- MicroSim_TQC5OX/MicroSim_TQC5OX/Form1.cs
+++ MicroSim_TQC5OX/MicroSim_TQC5OX/Form1.cs
@@ -14,6 +14,8 @@ namespace MicroSim_TQC5OX
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
@@ -24,6 +26,51 @@ namespace MicroSim_TQC5OX
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    private void SimStep(int year, Person person)
+                    {
+                        if (!person.IsAlive) return;
+
+                        byte age = (byte)(year - person.BirthYear);
+
+                        double pDeath = (from x in DeathProbabilities
+                                         where x.Gender == person.Gender && x.Age == age
+                                         select x.P).FirstOrDefault();
+                       
+                        if (rng.NextDouble() <= pDeath)
+                            person.IsAlive = false;
+
+                        if (person.IsAlive && person.Gender == Gender.Female)
+                        {
+                            double pBirth = (from x in BirthProbabilities
+                                             where x.Age == age
+                                             select x.P).FirstOrDefault();
+                           
+                            if (rng.NextDouble() <= pBirth)
+                            {
+                                Person újszülött = new Person();
+                                újszülött.BirthYear = year;
+                                újszülött.NbrOfChildren = 0;
+                                újszülött.Gender = (Gender)(rng.Next(1, 3));
+                                Population.Add(újszülött);
+                            }
+                        }
+                    }
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<DeathProbability> GetDeathProbabilities(string csvpath)
