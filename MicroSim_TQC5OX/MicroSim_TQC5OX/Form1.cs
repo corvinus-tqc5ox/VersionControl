1da6893f2e48c8c13e5dab699d8823acae0e865e
User <User@DESKTOP-VMHFV0H>
2021-12-03 10:10:15 +0100

    Add classes and lists

---
+++ MicroSim_TQC5OX/MicroSim_TQC5OX/Form1.cs
@@ -0,0 +1,24 @@
+﻿using MicroSim_TQC5OX.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicroSim_TQC5OX
+{
+    public partial class Form1 : Form
+    {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
